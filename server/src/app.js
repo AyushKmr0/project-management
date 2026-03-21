a33178cc3fb66b2a27ad0242a234c3d586ef31ac
Ayush <yeashr007@gmail.com>
2026-03-22 01:02:19 +0530

    initialize server and database connection setup

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./utils/ApiError.js";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URI],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	}),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(errorMiddleware);
+
+// routers import
+import authRouter from "./router/user.route.js";
+
+// routers declaration
+app.use("/api/v1/auth", authRouter);
+
+export { app };
---
+++ server/src/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import { connectDB } from "./database/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+	path: "./.env",
+});
+
+// Handle sync errors
+process.on("uncaughtException", (error) => {
+	console.error(`Uncaught Exception: ${error.message}`);
+	process.exit(1);
+});
+
+let server;
+
+connectDB()
+	.then(() => {
+		const port = process.env.PORT || 4000;
+
+		server = app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+
+		server.on("error", (error) => {
+			console.error("Server error:", error);
+			process.exit(1);
+		});
+	})
+	.catch((error) => {
+		console.error("MONGODB connection failed!!", error);
+		process.exit(1);
+	});
+
+// Handle async errors
+process.on("unhandledRejection", (error) => {
+	console.error(`Unhandled Rejection: ${error.message}`);
+
+	if (server) {
+		server.close(() => {
+			process.exit(1);
+		});
+	} else {
+		process.exit(1);
+	}
+});
